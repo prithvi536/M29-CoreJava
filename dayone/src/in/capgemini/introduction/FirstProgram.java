8574c4c1e9278f20cf51ebaaa0240367217c1eee
helloworld325 <patilprithvi536@gmail.com>
2023-04-04 11:06:09 +0530

    first commit

---
+++ dayone/src/in/capgemini/introduction/FirstProgram.java
@@ -0,0 +1,11 @@
+package in.capgemini.introduction;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		// TODO Auto-generated method stub
+
+	}
+
+}
