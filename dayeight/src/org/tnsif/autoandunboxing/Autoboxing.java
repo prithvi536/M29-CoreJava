7f6b1a2ffe21dfb8ab9cecfa5a5007145ab8bf4f
helloworld325 <patilprithvi536@gmail.com>
2023-04-27 17:05:43 +0530

    autoboxing and unboxing

---
+++ dayeight/src/org/tnsif/autoandunboxing/Autoboxing.java
@@ -0,0 +1,14 @@
+package org.tnsif.autoandunboxing;
+
+public class Autoboxing {
+
+	public static void main(String[] args) {
+		//primitive variable
+		int num = 1045;
+		
+		//auto boxing
+		//converting primitive to object type
+		Integer x = num;
+		System.out.println("Value of x is "+x);
+	}
+}
---
+++ dayeight/src/org/tnsif/autoandunboxing/Unboxing.java
@@ -0,0 +1,16 @@
+package org.tnsif.autoandunboxing;
+
+public class Unboxing {
+
+	public static void main(String[] args) {
+
+		//primitive
+		Character c = 'A';
+		
+		//unboxing
+		char ch = c;
+		
+		System.out.println(ch);
+		
+	}
+}
